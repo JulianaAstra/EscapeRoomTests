a55dd170083e9f0aec566389466adf566fb46e8b
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2025-10-31 11:25:38 +0300

    init

---
+++ src/test/java/tests/EscapeRoomTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.open;
+
+public class EscapeRoomTest extends TestBase {
+
+    @Test
+    void test() {
+        open("");
+    }
+}
